aaac488d0eb15c41651f2c566b1aa91b09433103
Gergely Nemeth <mail@nemethgergely.com>
2020-12-27 18:51:41 -0800

    feat(projects): add express

---
+++ apps/rush-express/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(-1)
+  }
+})
