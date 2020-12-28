4a0daafdbb713c9679e0e92395f6b7176a6fa0ed
Gergely Nemeth <mail@nemethgergely.com>
2020-12-27 19:42:14 -0800

    feat(libs): add dummy math lib

--- apps/rush-express/index.js
+++ apps/rush-express/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const math = require('@gergelyke/rush-math')
 const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('hello world' + math(1, 2))
 })
 
 app.listen(3000, (err) => {
